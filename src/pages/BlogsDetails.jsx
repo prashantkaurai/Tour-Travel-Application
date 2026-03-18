18824581194f991f69dcba0faf0eade6541b7249
PRASHANT KAURAI <153915493+prashantkaurai@users.noreply.github.com>
2026-03-18 22:28:15 +0530

    Update BlogsDetails.jsx

--- src/pages/BlogsDetails.jsx
+++ src/pages/BlogsDetails.jsx
@@ -11,14 +11,11 @@ const BlogsDetails = (props) => {
   return (
     <div className=" pt-20">
       <div className="h-[300px] overflow-hidden">
-        <img src={image} alt={title}
-          className="mx-auto h-[300px] w-full object-cover transition duration-700 hover:scale-110"
-        />
+        <img src={image} alt={title} className="mx-auto h-[300px] w-full object-cover transition duration-700 hover:scale-110"/>
       </div>
       <div className="container ">
         <p className="text-slate-600 text-sm py-3">
-          {" "}
-          written by {author} on {date}
+          {" "} written by {author} on {date}
         </p>
         <h1 className="text-2xl font-semibold">{title}</h1>
         <p>{description}</p>
