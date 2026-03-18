1b8d0317eb2eda2f63028cdcca8a3bc4cf470f6b
PRASHANT KAURAI <153915493+prashantkaurai@users.noreply.github.com>
2026-03-18 22:31:51 +0530

    Update BlogCard.jsx

--- src/components/Blogs/BlogCard.jsx
+++ src/components/Blogs/BlogCard.jsx
@@ -4,20 +4,14 @@ import { Link } from "react-router-dom";
 const BlogCard = ({ image, date, title, description, author }) => {
   return (
     <>
-      <Link
-        to={`/blogs/${title}`}
-        onClick={() => {
+      <Link to={`/blogs/${title}`} onClick={() => {
           window.scrollTo(0, 0);
         }}
         state={{ image, date, title, description, author }}
       >
         <div className="p-4 shadow-lg transition-all duration-500 hover:shadow-xl dark:bg-slate-950 dark:text-white">
           <div className="overflow-hidden">
-            <img
-              src={image}
-              alt="No image"
-              className="mx-auto h-[250px] w-full object-cover transition duration-700 hover:skew-x-2 hover:scale-110"
-            />
+            <img src={image} alt="No image" className="mx-auto h-[250px] w-full object-cover transition duration-700 hover:skew-x-2 hover:scale-110"/>
           </div>
           <div className="flex justify-between pt-2 text-slate-600">
             <p>{date}</p>
