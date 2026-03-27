b5e8170f251f056594b0e21dfc0b42838b87dcdc
PRASHANT KAURAI <153915493+prashantkaurai@users.noreply.github.com>
2026-03-27 20:04:18 +0530

    Refactor iframe attributes in Location component

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -13,12 +13,7 @@ const Location = () => {
           <div className="rounded-xl ">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1200.3047030095802!2d75.88848756812295!3d22.728392976990826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3962fd47d9e84ebd%3A0xe79cb0dac2dba1c8!2sBank%20of%20Maharashtra%20-%20Palasia%20Branch!5e1!3m2!1sen!2sin!4v1773733416427!5m2!1sen!2sin"
-              width="100%"
-              height="360"
-              allowfullscreen=""
-              loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
-              style={{ borderRadius: "20px" }}
+              width="100%" height="360" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" style={{ borderRadius: "20px" }}
             ></iframe>
           </div>
         </div>
